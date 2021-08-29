a4806ada50ddd16b5be3593f9ac89caafb6572c4
pavel1717 <vilkovpavelvilkov@gmail.com>
2021-08-29 13:43:04 +0700

    FirstCommit

---
+++ ToDoList/Models/User.cs
@@ -0,0 +1,15 @@
+﻿
+namespace ToDoList.Models;
+public class User
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Patronic { get; set; }
+
+    public string UserInfo()
+    {
+        return $"ID: {this.ID} Surname: {this.Surname} Name: {this.Name} Patronic: {this.Patronic}";
+    }
+
+}
