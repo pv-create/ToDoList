63e17fde20c536c081ab88dd3bb1b2a53f5f354a
Павел Вилков <pavel_vilkov17@mail.ru>
2021-09-17 23:11:07 +0700

    commit

--- ToDoList/Models/User.cs
+++ ToDoList/Models/User.cs
@@ -6,22 +6,39 @@ public class User
     public string Name { get; set; }
     public string Surname { get; set; }
     public string Patronic { get; set; }
-    public string Email { get; set; }
+    public string Number { get; set; }
 
     public User()
     {
-        this.ID = 0;
+        this.ID = 1;
         this.Name = "";
         this.Surname = "";
         this.Patronic = "";
-        this.Email = "";
+        this.Number = "";
+    }
+
+    public User(int ID, string Name, string Surname, string Patronic, string Email)
+    {
+
+        this.ID = ID;
+        this.Name = Name;
+        this.Surname = Surname;
+        this.Patronic = Patronic;
+        this.Number = Email;
     }
 
     public string UserInfo()
     {
+        IDCheck(this.ID);
         return $"ID: {this.ID} Surname: {this.Surname} Name: {this.Name} Patronic: {this.Patronic}";
     }
 
-
+    public void IDCheck(int ID)
+    {
+        if(ID<1)
+        {
+            throw new ArgumentOutOfRangeException("ID");
+        }
+    }
 
 }
---
+++ UserTest/UserTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList;
+
+namespace UserTest;
+[TestClass]
+public class UserTest
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        int ID = 1;
+        string Name = "ѕавел";
+        string Surname = "¬илков";
+        string Patronic = "ёрьевич";
+        string Email = "pavel_vilkov17@mail.ru";
+
+
+    }
+}
